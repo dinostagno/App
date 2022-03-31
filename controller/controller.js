5e19c8d6c333b3522ed1330729b2403770f28c54
Dino Stagno <85387888+dinostagno@users.noreply.github.com>
2022-03-31 20:05:55 -0300

    Update controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -1,4 +1,4 @@
-function getValues() {
+/*function getValues() {
   fetch('https://dinostagno-dev-ed.my.salesforce.com/services/apexrest/Accounts/0012E00002e4HURQA2', {
     method: 'GET',
     headers: {
@@ -17,4 +17,27 @@ function getValues() {
     .catch(function(err) {
     console.error(err);
   });
+}*/
+function getValues() {
+    fetch('https://login.salesforce.com/services/oauth2/token', {
+            method: 'POST',
+            body: JSON.stringify({
+                grant_type: password,
+                client_id: '3MVG9ZF4bs_.MKuhz4g8P5ekGrXZkNs0a2sba0CwY.6lhSXpZM4H.CM6okSQmxTiF0DMhGyMza2a_E4zfHbKn',
+                client_secret: '5CC14D5C61AB61C49E16DE550455A03AFB12B578F88B32D548BD5585F5707E54',
+                username: 'dpaolostagno@gmail.com',
+                password: 'ArchBash@2022e4P0ast5TIaxNmQgo5r1hO0Mh'
+            }),
+
+        })
+        .then(function(response) {
+            console.log('response =', response);
+            return response.json();
+        })
+        .then(function(data) {
+            console.log('data = ', data);
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
 }
