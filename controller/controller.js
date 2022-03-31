5e531fb0f14d541c34e58aaefce4e46731acf05c
Dino Stagno <85387888+dinostagno@users.noreply.github.com>
2022-03-31 20:34:23 -0300

    Update controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -21,13 +21,14 @@
 function getValues() {
     fetch('https://login.salesforce.com/services/oauth2/token', {
             method: 'POST',
+            mode: 'no-cors',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*'
             },
             body: JSON.stringify({
-                grant_type: 'password',
+                grant_type: password,
                 client_id: '3MVG9ZF4bs_.MKuhz4g8P5ekGrXZkNs0a2sba0CwY.6lhSXpZM4H.CM6okSQmxTiF0DMhGyMza2a_E4zfHbKn',
                 client_secret: '5CC14D5C61AB61C49E16DE550455A03AFB12B578F88B32D548BD5585F5707E54',
                 username: 'dpaolostagno@gmail.com',
