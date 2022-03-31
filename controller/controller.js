49065694390fc8e7660db4b1f471f7c0fbc3e8da
Dino Stagno <85387888+dinostagno@users.noreply.github.com>
2022-03-31 05:39:21 -0300

    Update controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -1,7 +1,20 @@
-function logInOrg(callback) {
-    let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
-    let response = await fetch(url);
-    let commits = await response.json(); // read response body and parse as JSON
-    alert(commits[0].author.login);
-}
+function getValues() {
+  fetch('https://dinostagno-dev-ed.my.salesforce.com/services/apexrest/Accounts/0012E00002e4HURQA2', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer 00D2E000000mpVn!ARIAQIBNvPOAxQMeDu6yC_ovxUjqYMkli_h34KOfuoJ69Nbxmctyh5orgmF4UhUK9yqHJjFsnrrvJFZWVFRjsJXjUpPfQQ7t'
+    },
 
+  })
+    .then(function(response) {
+    console.log('response =', response);
+    return response.json();
+  })
+    .then(function(data) {
+    console.log('data = ', data);
+  })
+    .catch(function(err) {
+    console.error(err);
+  });
+}
